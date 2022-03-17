87fb5a3d29b2edd1e373755facab4d863ac8b8d9
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2022-03-17 11:47:42 +0100

    testing the react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/compotest';
+
 
 function App() {
   return (
@@ -8,16 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          lets go baby
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Test />
       </header>
     </div>
   );
