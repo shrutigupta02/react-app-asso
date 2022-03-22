af2b04337c805d7bae87ded66d9a18feccb75401
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2022-03-22 14:10:26 +0100

    login page styling

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import './login.css'
 
-export default class Login extends Component {
+export type loginState = {
+  isCreatingAccount: Boolean
+}
+
+export default class Login extends React.Component<{},loginState>{
+ 
+  state = {
+    isCreatingAccount: false
+  }
+
+  handleIsCreatingAccount = () => {
+    let isCreatingAccount = !this.state.isCreatingAccount
+    this.setState({isCreatingAccount})
+  }
+
   render() {
-    return (
-      <div>
-        <div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div><div>login</div>
-      </div>
-    )
+    if(this.state.isCreatingAccount == false){
+      return (
+        <div className="loginPage">
+          <form action="" method="get" className='loginContainer'>
+            <input type="email" name='email' id='email' placeholder='      email' className='loginInput'></input> 
+            <input type="password" name='password' id='password' placeholder='     mot de passe' className='loginInput'/>
+            <input type="submit" value='se connecter' className='loginInput confirmInput'/>
+            <div></div>
+            <p onClick={() => this.handleIsCreatingAccount() }>créer un compte</p>
+          </form>
+        </div>
+      )
+    } else {
+      return (
+        <div className="loginPage">
+          <form action="" method="get" className='loginContainer'>
+            <input type="email" name='email' id='email' placeholder='      email' className='loginInput'></input> 
+            <input type="password" name='password' id='password' placeholder='     mot de passe' className='loginInput'/>
+            <input type="password" name='confirmpassword' id='confirmpassword' placeholder='     confirmer le mot de passe' className='loginInput'/>
+            <input type="submit" value='créer un compte' className='loginInput confirmInput'/>
+            <div></div>
+            <p onClick={() => this.handleIsCreatingAccount() }>se connecter</p>
+          </form>
+        </div>
+      )
+    }
   }
 }
